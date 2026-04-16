8f60a40eca25a1c12431fb904bcb3bad4bba9a46
Ishaan Pastariya <skullcrusherarmy@gmail.com>
2026-04-17 00:01:27 +0530

    final fix binding

--- server.js
+++ server.js
@@ -61,6 +61,6 @@ console.log("PORT VALUE:", process.env.PORT);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
