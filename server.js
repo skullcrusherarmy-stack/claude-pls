cd574cd56d7ee5a01adebce1004b8956e27d0dbf
Ishaan Pastariya <skullcrusherarmy@gmail.com>
2026-04-17 00:13:35 +0530

    restore real route

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ console.log("PORT VALUE:", process.env.PORT);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
